ccda40dad507cd887a2eaca8de411e232e1aff52
CrisLP <cristianlupasco2004@gmail.com>
2025-10-26 12:31:16 +0200

    Removed Lab1 files from Laboratory2 branch

--- lab1/lab1/INotificationSender.cs
+++
@@ -1,8 +0,0 @@
-namespace SOLIDDemo
-{
-    // Interface for sending notifications (used for Open/Closed Principle)
-    public interface INotificationSender
-    {
-        void Send(string message, string recipient);
-    }
-}
